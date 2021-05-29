578e666022681a6c6dc710c87c806111c5fa6dbd
ZyadTarek <zyad.tarek175@gmail.com>
2021-05-29 15:57:43 +0300

    Add ServerSide Project

---
+++ Server/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerSide
+{
+	public partial class Form1 : Form
+	{
+		TcpListener server;
+		Byte[] ipbytes;
+		IPAddress ipaddress;
+		int portNo;
+		Socket connection;
+		NetworkStream nstream;
+		BinaryReader reader;
+		BinaryWriter writer;
+		bool isServerRunning;
+		public string word;
+		public secretword h;
+		string player;
+
+		//string sentString;
+		public TcpListener Server { get => server; set => server = value; }
+		public Byte[] Ipbytes { get => ipbytes; set => ipbytes = value; }
+		public IPAddress IpAddress { get => ipaddress; set => ipaddress = value; }
+		public int PortNo { get => portNo; set => portNo = value; }
+		public Socket Connection { get => connection; set => connection = value; }
+		public string PlayerIP { get; set; }
+		public ArrayList clients { get; set; }
+		public static string wordcategory { get; set; }
+		public static string worddifficulty { get; set; }
+		public Form1()
+		{
+			InitializeComponent();
+			Ipbytes = new Byte[] { 127,0,0,1};
+			IpAddress = new IPAddress(Ipbytes);
+			PortNo = 7777;
+			Server = new TcpListener(IpAddress, PortNo);
+			clients = new ArrayList();
+			clients.Add(IpAddress.ToString());
+			comboBox1.Items.Add("Fruits");
+			comboBox1.Items.Add("Colors");
+			comboBox1.Items.Add("Animals");
+			comboBox2.Items.Add("Easy");
+			comboBox2.Items.Add("Medium");
+			comboBox2.Items.Add("Hard");
+		}
+
+		//connnect Async
+		/*private async void connectAsync()
+		{
+			await Task.Run(async () =>
+			{
+				Server.Start();
+				MessageBox.Show("Server Started..");
+				Connection = Server.AcceptSocket();
+				
+		        MessageBox.Show(Connection.Connected.ToString());
+				var clientIp = Connection.RemoteEndPoint as IPEndPoint;
+				MessageBox.Show(clientIp.ToString());
+				clients.Add(clientIp.Address);
+
+				nstream = new NetworkStream(Connection);
+				h = new Hangman(this);
+				sendWord();
+				h.ShowDialog();
+				await RecieveAsync();
+
+				//MessageBox.Show(Connection.Connected.ToString());
+
+
+				isServerRunning = true;
+				//MessageBox.Show("connection accepted");
+				//await RecieveAsync();
+			});
+		}*/
+
+		private void button3_ClickAsync(object sender, EventArgs e)
+		{
+			
+			backgroundWorker2.RunWorkerAsync();
+			wordcategory = comboBox1.SelectedItem.ToString();
+			worddifficulty = comboBox2.SelectedItem.ToString();
+			h = new secretword(this);
+			///h.startGame();
+			//connectAsync();
+
+		}
+		public void sendChar()
+		{
+			//MessageBox.Show(nstream.ToString());
+			try
+			{
+				//nstream = new NetworkStream(Connection);
+				writer = new BinaryWriter(nstream);
+
+				if (nstream.CanWrite)
+				{
+					writer.Write(h.CharClicked);
+					//MessageBox.Show(h.currentWord);
+					//richTextBox1.Text = "";
+					MessageBox.Show("Character Sent to client x");
+					//writer.Close();
+				}
+			}
+			catch (Exception) { MessageBox.Show("Client x is not exist."); }
+		}
+		public void sendPlayerIP()
+		{
+			try
+			{
+				//nstream = new NetworkStream(Connection);
+				writer = new BinaryWriter(nstream);
+
+				if (nstream.CanWrite)
+				{
+					writer.Write(PlayerIP.ToString());
+					//MessageBox.Show(h.currentWord);
+					//richTextBox1.Text = "";
+					MessageBox.Show("IP Sent to client x");
+					//writer.Close();
+				}
+			}
+			catch (Exception) { MessageBox.Show("Player IP is not exist."); }
+
+		}
+
+		public void sendWord()
+		{
+			//MessageBox.Show(nstream.ToString());
+			try
+			{
+				//nstream = new NetworkStream(Connection);
+				writer = new BinaryWriter(nstream);
+
+				if (nstream.CanWrite)
+				{
+					writer.Write($"{h.currentWord},{wordcategory},{worddifficulty}");
+					//MessageBox.Show(h.currentWord);
+					//richTextBox1.Text = "";
+					MessageBox.Show("Message Sent to client x");
+					//writer.Close();
+				}
+			}
+			catch (Exception) { MessageBox.Show("Client x is not exist."); }
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				//nstream = new NetworkStream(Connection);
+				writer = new BinaryWriter(nstream);
+
+				if (nstream.CanWrite)
+				{
+					writer.Write(h.currentWord);
+					//richTextBox1.Text = "";
+					MessageBox.Show("Message Sent to client x");
+					//writer.Close();
+				}
+			}
+			catch (Exception) { MessageBox.Show("Client x is not exist."); }
+		}
+		public void RecieveClientIP()
+		{
+
+			if (isServerRunning && InvokeRequired)
+			{
+				reader = new BinaryReader(nstream);
+
+				  player = reader.ReadString();
+
+				Invalidate();
+				MessageBox.Show(player);
+				MessageBox.Show("IP Recieved from client x");
+				clients.Add(player);
+				foreach (string ip in clients)
+				{
+					MessageBox.Show("ip :" + ip);
+
+				}
+				MessageBox.Show(clients.Count.ToString());
+			}
+		}
+		public void Recieve()
+		{
+			//richTextBox2.Text = "";
+
+			//nstream = new NetworkStream(Connection);
+			while (isServerRunning && InvokeRequired)
+			{
+			reader = new BinaryReader(nstream);
+
+				word = reader.ReadString();
+				 h.RecieveChar();
+				
+				Invalidate();
+				//MessageBox.Show(word);
+				MessageBox.Show("Message Recieved from client x");
+
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			//Recieve();
+			//reader.Close();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (Connection.Connected)
+				{
+					//reader.Close();
+					nstream.Close();
+					Connection.Shutdown(SocketShutdown.Both);
+					Connection.Close();
+					MessageBox.Show("Connection is Closed..");
+				}
+			}
+			catch (Exception exe) { MessageBox.Show(exe.ToString()); }
+		}
+
+		private void backgroundWorker2_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+		{
+			//Server Connection
+			Server.Start();
+			MessageBox.Show("Server Started..");
+			Connection = Server.AcceptSocket();
+			nstream = new NetworkStream(Connection);
+			h = new secretword(this);
+			sendWord();
+			isServerRunning = true;
+			//MessageBox.Show(Connection.Connected.ToString());
+			//var clientIp = Connection.RemoteEndPoint as IPEndPoint;
+			//MessageBox.Show(clientIp.ToString());
+			//clients.Add(clientIp.Address);
+			//form.clients.Add(form.IpAddress.ToString());
+			
+			reader = new BinaryReader(nstream);
+			writer = new BinaryWriter(nstream);
+
+			if (h.currentWord != string.Empty)
+				backgroundWorker1.RunWorkerAsync();
+
+
+			h.ShowDialog();
+			//clients.Count
+
+
+			//RecieveClientIP();
+			//sendWord();
+			//h.ShowDialog();
+
+
+			//foreach (string ip in clients)
+			//MessageBox.Show("IP: " + ip);
+			//MessageBox.Show(Connection.Connected.ToString());
+
+
+
+
+
+			/*Server.Start();
+			MessageBox.Show("Server Started..");
+			Connection = Server.AcceptSocket();
+			nstream = new NetworkStream(Connection);
+			isServerRunning = true;
+			MessageBox.Show("connection accepted");
+			backgroundWorker1.RunWorkerAsync();*/
+		}
+		/*	private async Task RecieveAsync()
+			{
+				await Task.Run(() => { Recieve();});
+			}*/
+		private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+		{
+	
+			Recieve();
+
+		}
+		private async Task DoWorkAsync()
+		{
+			 await Task.Run(() => {
+				Thread.Sleep(1000);
+				 //return "Done with work!";
+				 MessageBox.Show("Hello");
+			});
+
+		}
+		private async void button5_Click(object sender, EventArgs e)
+		{
+			//this.Text = 
+				await DoWorkAsync();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			CheckForIllegalCrossThreadCalls = false;
+
+		}
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Server/Form1.designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace ServerSide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe Print", 13.875F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button3.Location = new System.Drawing.Point(480, 454);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 76);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_ClickAsync);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(434, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 85);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Secret Word";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe Print", 7.875F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(536, 240);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(338, 45);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Segoe Print", 7.875F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(536, 312);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(336, 45);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(347, 229);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 57);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(347, 301);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 57);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Difficulty";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HangmanD_7.Properties.Resources.Rd3892fb608c5d4569bf44ce5f1622a6e;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(406, 392);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1180, 676);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Start Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
